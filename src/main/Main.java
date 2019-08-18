1e4f7cf8736215afb22dc22eb69cf5d72ab9aade
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-08-18 07:21:53 -0700

    check whether the no is even or odd.

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,11 +10,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         int a = obj.nextInt();
-        if (a < 0)
-            System.out.println("Negative");
-        else if (a == 0)
-            System.out.println("Zero");
+        if (a % 2 == 0)
+            System.out.println("even");
         else
-            System.out.println("Positive");
+            System.out.println("odd");
     }
     }
