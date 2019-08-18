5037ce32fc1d7ff41940c6e69ad167ac92edeca1
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-08-18 08:35:57 -0700

    initialize the variable

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,14 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         int a = obj.nextInt();
-        int c = 0;
-        for (int i = 1; i <= a; i++) {
-            if (a % i == 0)
-                c = c + 1;
-        }
-        if (c == 2)
-            System.out.println(a + " is a prime number.");
-        else
-            System.out.println((a + " is not a prime number."));
+        int b = obj.nextInt();
+
     }
 }
