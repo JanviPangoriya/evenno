db784f2f8142c99e33d89b1ef495ed383a8bc972
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-08-18 07:11:27 -0700

    print the output in the desired output . ____+University+______

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,11 +9,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        int a = obj.nextInt();
-        int i;
-        for (i = 1; i <= 10; i++) {
-            System.out.println(a + " X " + i + " = " + (a * i));
-            // a X i=z
+        String a = obj.nextLine();
+        String b = obj.nextLine();
+        System.out.println(a + " University " + b);
     }
     }
-}
